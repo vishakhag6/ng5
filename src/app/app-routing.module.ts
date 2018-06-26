990094af1fa2bcfd2276598fcbdd91a19daea2cf
vishakha nehe <vishakhag6@gmail.com>
2018-06-26 14:34:57 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
+import { HomeComponent } from '../app/home/home.component';
+import { AboutComponent } from '../app/about/about.component';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, stagger, query, animate, keyframes } from '@angular/animations';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('goalsAnimation', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0}), {optional: true}),
+
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateX(-75%)', offset: 0}),
+            style({opacity: 0.5, transform: 'translateX(35px)', offset: .3}),
+            style({opacity: 1, transform: 'translateX(0)', offset: 1})
+          ]))]), {optional: true}),
+
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateX(0)', offset: 0}),
+            style({opacity: 0.5, transform: 'translateX(35px)', offset: .3}),
+            style({opacity: 0, transform: 'translateX(-75%)', offset: 1})
+          ]))]), {optional: true})
+      ])
+    ])
+  ]
+})
+export class HomeComponent implements OnInit {
+
+  itemCount: number;
+  btnText: string = 'Add an item';
+  inputText: string = 'My first goal is to learn everything';
+  goalsArr = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.itemCount = this.goalsArr.length;
+  }
+
+  addItem() {
+    if(this.inputText !== '' && this.goalsArr.indexOf(this.inputText) == -1) {
+      this.goalsArr.push(this.inputText);
+    }
+
+    this.inputText = '';
+    this.itemCount = this.goalsArr.length;
+  }
+
+  removeItem(i) {
+    this.goalsArr.splice(i, 1);
+  }
+}
